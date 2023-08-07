d41b72a47626c8d43f2570418690d1c6178d9f1f
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-08-07 19:46:30 +0000

    baseModule

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines the class BaseModel"""
+import uuid
+import datetime
 
 
 
@@ -10,3 +12,18 @@ class BaseModel:
           created_at: datetime
           updated_at: datetime
     """
+    def __init__(self):
+        """initializes attrinutes of the class"""
+        self.id = uuid.uuid4()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name,
+                                     self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """Updates the attribute updated_at with the current datetime"""
+        self.updated_at = datetime.datetime.now()
