b1fd011396b02b607dbfb702d22abcbf2615c8f8
ela-hidri <elahidri.eh@gmail.com>
2023-08-07 13:52:10 -0700

    finishing base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,16 +14,24 @@ class BaseModel:
     """
     def __init__(self):
         """initializes attrinutes of the class"""
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name,
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id,
                                      self.__dict__)
 
     def save(self):
         """Updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        string = self.__dict__.copy()
+        string['__class__'] = self.__class__.__name__
+        string['created_at'] = self.created_at.isoformat()
+        string['updated_at'] = self.updated_at.isoformat()
+        return string
+        
