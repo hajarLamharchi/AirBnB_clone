99b75d707059d814f0f6126314a36ff0e2330216
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-08-07 14:43:51 +0000

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the class BaseModel"""
+
+
+
+class BaseModel:
+    """This class defines all common attributes/methods for other classes:
+       Attributes:
+          id: string
+          created_at: datetime
+          updated_at: datetime
+    """
