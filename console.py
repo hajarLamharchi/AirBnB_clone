3f6b7572e3026bd0bd8af5d2183906facbcd1b8b
ela-hidri <elahidri.eh@gmail.com>
2023-08-08 15:29:02 -0700

    adding create to console.py

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
     define a program that contains the entry point of the command interpreter
 """
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +24,18 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         print
+
+    def do_create(self, clas):
+        'Creates a new instance of BaseModel, saves it'
+        if clas:
+            if clas == 'BaseModel':
+                new = BaseModel()
+                new.save()
+                print(new.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
 
 if __name__ == '__main__':
